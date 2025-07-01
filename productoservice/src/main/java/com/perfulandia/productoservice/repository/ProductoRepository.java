3c427e579f2953b65bf9b368e2b2c8b2a7abed3c
Matías Loyola <matiazloyola@gmail.com>
2025-07-01 02:58:13 -0400

    Delete productoservice directory

--- productoservice/src/main/java/com/perfulandia/productoservice/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.perfulandia.productoservice.repository;
-
-import com.perfulandia.productoservice.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
